247928a10a5cd26521b90401a5972e2f0898c4cb
Jun <2499932100@qq.com>
2019-03-05 02:22:28 +0800

    Create 目录结构说明.js

---
+++ 目录结构说明.js
@@ -0,0 +1,27 @@
+// ├── public
+// │   ├── index.html             // html模板
+// ├── src                        // 源代码
+// │   ├── api                    // 所有请求
+// │   ├── assets                 // 主题 字体等静态资源
+// │   ├── components             // 全局公用组件
+// │   ├── directive              // 全局指令
+// │   ├── filtres                // 全局 filter
+// │   ├── icons                  // 项目所有 svg icons
+// │   ├── lang                   // 国际化 language
+// │   ├── mock                   // 项目mock 模拟数据
+// │   ├── router                 // 路由
+// │   ├── store                  // 全局 store管理
+// │   ├── styles                 // 全局样式（风格和基础色的配置）
+// │   ├── utils                  // 全局公用方法
+// │   ├── vendor                 // 第三方工具（Export2Excel.js:导出为Excel，Export2Zip.js:导出为zip）
+// │   ├── views                  // 视图组件
+// │   ├── App.vue                // 入口页面
+// │   ├── main.js                // 入口 加载组件 初始化等
+// │   └── permission.js          // 权限管理
+// ├── static                     // 第三方不打包资源（重量级插件）
+// │   └── Tinymce                // 富文本
+// ├── .babelrc.config.js         // babel-loader 配置
+// ├── .gitignore                 // git 忽略项
+// ├── favicon.ico                // favicon图标
+// ├── index.html                 // html模板
+// └── package.json               // package.json
