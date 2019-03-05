75641b2e2fdcc9e2d7816122d1efdbdef88a0d62
Jun <2499932100@qq.com>
2019-03-05 23:07:01 +0800

    添加配置文件
    使用链式操作对webpack规则进行了修改

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+module.exports = {
+    chainWebpack:config => {
+        // 修改内部svg规则
+        const svg = config.module.rule('svg');
+        svg.export.add(resolve('src/icons'))
+
+        // 新增规则，use为规则命名，loader表示使用的加载器，tap表示配置选项
+        const svgSpriteLoader = config.module.rule('svg-sprite')
+        svgSpriteLoader
+        .test(/\.svg$/)
+        .include.add(resolve('src/icons/svg')).end()
+        .use('svg-sprite-loader')
+        .loader('svg-sprite-loader')
+        .options({
+            symbolId: 'icon-[name]'
+        })
+    }
+}
