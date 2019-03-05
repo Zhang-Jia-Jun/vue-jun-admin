99aa9462b5f89a3e816dba4b15ab5b52fb7cbb4e
Jun <2499932100@qq.com>
2019-03-06 00:18:23 +0800

    美化Login组件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -10,7 +10,13 @@
         <el-input placeholder="用户账号" v-model="loginAccount.username"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-input placeholder="用户密码" v-model="loginAccount.password"></el-input>
+        <span class="svg-container">
+          <icon icon-name="lock_fill"></icon>
+        </span>
+        <el-input placeholder="用户密码" v-model="loginAccount.password" :type="this.pwdState ? '' : 'password'"></el-input>
+        <span class="show-pwd" @click="showPwd()">
+          <icon :icon-name="this.pwdState ? 'eye':'eye-close'"></icon>
+        </span>
       </el-form-item>
       <el-form-item>
         <el-button type="primary">Sign in</el-button>
@@ -30,43 +36,22 @@ export default {
       loginAccount: {
         username: "admin",
         password: "admin"
-      }
+      },
+      pwdState:false,
     };
+  },
+  computed:{
+  },
+  methods:{
+    showPwd(){
+      this.pwdState = !this.pwdState;
+    }
   }
 };
 </script>
 
-<style rel="stylesheet/scss" lang="scss">
-// 由于使用scoped会导致无法修改第三方样式，所以单独分离出来
+<style lang="scss" rel="stylesheet/scss" scoped>
 
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-    input {
-      background: transparent;
-      border: 0px;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: #eee;
-      height: 47px;
-    }
-  }
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-  .el-button {
-    width: 100%;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
 // 登录页背景颜色
 $bg: #2d3a4b;
 // icon颜色
@@ -95,6 +80,50 @@ $title-color: #eee;
       margin: 0 auto 40px auto;
       text-align: center;
     }
+    .svg-container{
+      line-height: 45px;
+      font-size: 1.5rem;
+      padding-left: 15px;
+      color:rgba($color: #fff, $alpha: .3)
+    }
+    .show-pwd{
+      line-height: 47px;
+      font-size: 1.3rem;
+      color: rgba($color: #fff, $alpha: .5);
+      width: 50px;
+      text-align: center;
+    }
+  }
+}
+</style>
+
+<style rel="stylesheet/scss" lang="scss">
+// 由于使用scoped会导致无法修改第三方样式，所以单独分离出来
+
+/* reset element-ui css */
+.login-container{
+  .el-input {
+    display: inline-block;
+    input {
+      background: transparent;
+      border: 0px;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: #eee;
+      height: 47px;
+    }
+  }
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+    & > div {
+      display: flex;
+    }
+  }
+  .el-button {
+    width: 100%;
   }
 }
 </style>
