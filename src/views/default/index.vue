ee88899aa2320b6d51adb018b97b9952a82eea77
Jun <2499932100@qq.com>
2019-03-08 20:40:43 +0800

    加入默认页面和404页面
    Signed-off-by: Jun <2499932100@qq.com>

---
+++ src/views/default/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="default">
+        {{this.$route.meta.title}}
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log(this.$route)
+    }
+}
+</script>
+
+<style>
+
+</style>
